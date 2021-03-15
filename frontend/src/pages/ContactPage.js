98cbf3607e216919380a77ffab6f247d93fd6dc2
Junghoon_Suk <sukjh87@gmail.com>
2021-03-15 17:10:19 +0800

    Minor adjust styling on Contact Page (sizing)

--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -18,9 +18,9 @@ const ContactPage = () => {
                         target="_blank"
                         rel="noreferrer"
                     >
-                        <i className="far fa-envelope fa-3x"></i>
+                        <i className="far fa-envelope fa-4x"></i>
                     </a>
-                    <h6>Email</h6>
+                    <h5>Email</h5>
                 </Col>
                 <Col className="mx-4">
                     <a
@@ -28,9 +28,9 @@ const ContactPage = () => {
                         target="_blank"
                         rel="noreferrer"
                     >
-                        <i className="fab fa-github-square fa-3x"></i>
+                        <i className="fab fa-github-square fa-4x"></i>
                     </a>
-                    <h6>GitHub</h6>
+                    <h5>GitHub</h5>
                 </Col>
             </Row>
         </Container>
