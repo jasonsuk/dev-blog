15ead1948f39744dd2504fbf8fcd0488581a7008
Junghoon_Suk <sukjh87@gmail.com>
2021-03-16 09:24:50 +0800

    Restyling Post component

--- frontend/src/components/Post.component.jsx
+++ frontend/src/components/Post.component.jsx
@@ -3,13 +3,26 @@ import { Container, Card, Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Post = ({ post }) => {
+    const postCardStyle = {
+        minHeight: '500px',
+        borderRadius: '15px',
+        margin: '2rem 0',
+        padding: '2rem 0',
+        textAlign: 'center',
+        background: '#f4f4f4',
+    };
+
+    const postBadgeStyle = {
+        margin: '1px 3px',
+        padding: '5px 10px',
+        borderRadius: '15px',
+        background: '#2b3e4f',
+        fontSize: '1rem',
+    };
+
     return (
         <>
-            <Card
-                style={{ minHeight: 500 }}
-                className="rounded my-2 p-2 text-center"
-                bg="white"
-            >
+            <Card style={postCardStyle}>
                 {post.image.length > 0 && (
                     <Link to={`/post/${post._id}`}>
                         <Card.Img
@@ -30,7 +43,7 @@ const Post = ({ post }) => {
                     <Container className="custom-badge">
                         {post.tags &&
                             post.tags.map((tag, i) => (
-                                <Badge key={i} pill variant="dark">
+                                <Badge key={i} style={postBadgeStyle}>
                                     {i <= 3 && tag}
                                 </Badge>
                             ))}
